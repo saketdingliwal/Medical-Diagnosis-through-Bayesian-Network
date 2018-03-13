a600272d3c76dd70d46886dafe744c47f9e3f636
saketdingliwal <saketdingliwal97@gmail.com>
2018-03-14 03:36:38 +0530

    changed data-structure

--- src/startup_code.cpp
+++ src/startup_code.cpp
@@ -12,25 +12,22 @@ using namespace std;
 
 // Our graph consists of a list of nodes where each node is represented as follows:
 class Graph_Node{
-
-private:
-	string Node_Name;  // Variable name 
+public:
+	string Node_Name;  // Variable name
 	vector<int> Children; // Children of a particular node - these are index of nodes in graph.
-	vector<string> Parents; // Parents of a particular node- note these are names of parents
+	vector<int> Parents; // Parents of a particular node- note these are names of parents
 	int nvalues;  // Number of categories a variable represented by this node can take
 	vector<string> values; // Categories of possible values
-	vector<float> CPT; // conditional probability table as a 1-d array . Look for BIF format to understand its meaning
+	vector<int> CPT; // conditional probability table as a 1-d array . Look for BIF format to understand its meaning
+	vector<int> updated_CPT;
 
-public:
+
 	// Constructor- a node is initialised with its name and its categories
-    Graph_Node(string name,int n,vector<string> vals)
+  Graph_Node(string name,int n,vector<string> vals)
 	{
 		Node_Name=name;
-	
 		nvalues=n;
 		values=vals;
-		
-
 	}
 	string get_name()
 	{
@@ -40,11 +37,11 @@ public:
 	{
 		return Children;
 	}
-	vector<string> get_Parents()
+	vector<int> get_Parents()
 	{
 		return Parents;
 	}
-	vector<float> get_CPT()
+	vector<int> get_CPT()
 	{
 		return CPT;
 	}
@@ -56,91 +53,55 @@ public:
 	{
 		return values;
 	}
-	void set_CPT(vector<float> new_CPT)
+	void set_CPT(vector<int> new_CPT)
 	{
 		CPT.clear();
 		CPT=new_CPT;
 	}
-    void set_Parents(vector<string> Parent_Nodes)
+  void set_Parents(vector<int> Parent_Nodes)
+  {
+    Parents.clear();
+    Parents=Parent_Nodes;
+  }
+  // add another node in a graph as a child of this node
+  int add_child(int new_child_index)
+  {
+    for(int i=0;i<Children.size();i++)
     {
-        Parents.clear();
-        Parents=Parent_Nodes;
+      if(Children[i]==new_child_index)
+          return 0;
     }
-    // add another node in a graph as a child of this node
-    int add_child(int new_child_index )
-    {
-        for(int i=0;i<Children.size();i++)
-        {
-            if(Children[i]==new_child_index)
-                return 0;
-        }
-        Children.push_back(new_child_index);
-        return 1;
-    }
-
-
-
+    Children.push_back(new_child_index);
+    return 1;
+  }
 };
 
 
  // The whole network represted as a list of nodes
 class network{
-
-	list <Graph_Node> Pres_Graph;
-
 public:
+	vector<Graph_Node> Pres_Graph;
 	int addNode(Graph_Node node)
 	{
 		Pres_Graph.push_back(node);
 		return 0;
 	}
-    
-    
 	int netSize()
 	{
 		return Pres_Graph.size();
 	}
-    // get the index of node with a given name
-    int get_index(string val_name)
+  // get the index of node with a given name
+  int get_index(string val_name)
+  {
+    int count=0;
+    for(int i=0;i<Pres_Graph.size();i++)
     {
-        list<Graph_Node>::iterator listIt;
-        int count=0;
-        for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++)
-        {
-            if(listIt->get_name().compare(val_name)==0)
-                return count;
-            count++;
-        }
-        return -1;
+      if(Pres_Graph[i].get_name().compare(val_name)==0)
+        return count;
+      count++;
     }
-// get the node at nth index
-    list<Graph_Node>::iterator get_nth_node(int n)
-    {
-       list<Graph_Node>::iterator listIt;
-        int count=0;
-        for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++)
-        {
-            if(count==n)
-                return listIt;
-            count++;
-        }
-        return listIt; 
-    }
-    //get the iterator of a node with a given name
-    list<Graph_Node>::iterator search_node(string val_name)
-    {
-        list<Graph_Node>::iterator listIt;
-        for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++)
-        {
-            if(listIt->get_name().compare(val_name)==0)
-                return listIt;
-        }
-    
-            cout<<"node not found\n";
-        return listIt;
-    }
-	
-
+    return -1;
+  }
 };
 
 network read_network()
@@ -148,127 +109,77 @@ network read_network()
 	network Alarm;
 	string line;
 	int find=0;
-  	ifstream myfile("alarm.bif"); 
-  	string temp;
-  	string name;
-  	vector<string> values;
-  	
-    if (myfile.is_open())
-    {
-    	while (! myfile.eof() )
-    	{
-    		stringstream ss;
-      		getline (myfile,line);
-      		
-      		
-      		ss.str(line);
-     		ss>>temp;
-     		
-     		
-     		if(temp.compare("variable")==0)
-     		{
-                    
-     				ss>>name;
-     				getline (myfile,line);
-                   
-     				stringstream ss2;
-     				ss2.str(line);
-     				for(int i=0;i<4;i++)
-     				{
-     					
-     					ss2>>temp;
-     					
-     					
-     				}
-     				values.clear();
-     				while(temp.compare("};")!=0)
-     				{
-     					values.push_back(temp);
-     					
-     					ss2>>temp;
-    				}
-     				Graph_Node new_node(name,values.size(),values);
-     				int pos=Alarm.addNode(new_node);
-
-     				
-     		}
-     		else if(temp.compare("probability")==0)
-     		{
-                    
-     				ss>>temp;
-     				ss>>temp;
-     				
-                    list<Graph_Node>::iterator listIt;
-                    list<Graph_Node>::iterator listIt1;
-     				listIt=Alarm.search_node(temp);
-                    int index=Alarm.get_index(temp);
-                    ss>>temp;
-                    values.clear();
-     				while(temp.compare(")")!=0)
-     				{
-                        listIt1=Alarm.search_node(temp);
-                        listIt1->add_child(index);
-     					values.push_back(temp);
-     					
-     					ss>>temp;
-
-    				}
-                    listIt->set_Parents(values);
-    				getline (myfile,line);
-     				stringstream ss2;
-                    
-     				ss2.str(line);
-     				ss2>> temp;
-                    
-     				ss2>> temp;
-                    
-     				vector<float> curr_CPT;
-                    string::size_type sz;
-     				while(temp.compare(";")!=0)
-     				{
-                        
-     					curr_CPT.push_back(atof(temp.c_str()));
-     					
-     					ss2>>temp;
-                       
-                        
-
-    				}
-                    
-                    listIt->set_CPT(curr_CPT);
-
-
-     		}
-            else
-            {
-                
-            }
-     		
-     		
-
-    		
-    		
-    	}
-    	
-    	if(find==1)
-    	myfile.close();
+	ifstream myfile("../dataset/alarm.bif");
+	string temp;
+	string name;
+	vector<string> values;
+  if (myfile.is_open())
+  {
+  	while (! myfile.eof() )
+  	{
+  		stringstream ss;
+    	getline (myfile,line);
+    	ss.str(line);
+   		ss>>temp;
+   		if(temp.compare("variable")==0)
+   		{
+ 				ss>>name;
+ 				getline (myfile,line);
+ 				stringstream ss2;
+ 				ss2.str(line);
+ 				for(int i=0;i<4;i++)
+ 				{
+ 					ss2>>temp;
+ 				}
+ 				values.clear();
+ 				while(temp.compare("};")!=0)
+ 				{
+ 					values.push_back(temp);
+ 					ss2>>temp;
+				}
+ 				Graph_Node new_node(name,values.size(),values);
+ 				int pos=Alarm.addNode(new_node);
+   		}
+   		else if(temp.compare("probability")==0)
+   		{
+ 				ss>>temp;
+ 				ss>>temp;
+        int index;
+        int parent_index;
+ 				index=Alarm.get_index(temp);
+        ss>>temp;
+        values.clear();
+ 				while(temp.compare(")")!=0)
+ 				{
+          parent_index=Alarm.get_index(temp);
+					Alarm.Pres_Graph[parent_index].add_child(index);
+ 					Alarm.Pres_Graph[index].Parents.push_back(parent_index);
+ 					ss>>temp;
+				}
+				getline (myfile,line);
+ 				stringstream ss2;
+ 				ss2.str(line);
+ 				ss2>> temp;
+ 				ss2>> temp;
+ 				while(temp.compare(";")!=0)
+ 				{
+					Alarm.Pres_Graph[index].CPT.push_back(0); // TODO assuming only -1 to be in input
+					Alarm.Pres_Graph[index].updated_CPT.push_back(0);
+ 					// 	curr_CPT.push_back(atof(temp.c_str()));
+ 					ss2>>temp;
+				}
+   		}
+      else
+      {
+      }
   	}
-  	
-  	return Alarm;
+  	if(find==1)
+  	myfile.close();
+	}
+	return Alarm;
 }
-
-
 int main()
 {
 	network Alarm;
 	Alarm=read_network();
-    
-// Example: to do something
-	cout<<"Perfect! Hurrah! \n";
-	
 }
-
-
-
-
-
